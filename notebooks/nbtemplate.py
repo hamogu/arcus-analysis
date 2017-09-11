6627be7aae225fd71037e360176594bc2b0dece4
hamogu <moritz.guenther@gmx.de>
2017-09-11 10:00:52 -0400

    Add header to more notebooks and convert ot Arcus() object
    This is an interative process. I do not go through all notebooks at once,
    but revise them whenever I look at them, thus this conversion is spread out
    over mutliple commits.

--- notebooks/nbtemplate.py
+++ notebooks/nbtemplate.py
@@ -1,7 +1,20 @@
+'''This module collects utilities for the Arcus notebooks.'''
+from __future__ import print_function
+
+import sys
+import os
 import subprocess
 from IPython.display import display, Markdown, HTML, Image
 
-__all__ = ['display_header']
+try:
+    import configparser # Py 3
+except ImportError:
+    import ConfigParser as configparser # Py 2
+
+__all__ = ['display_header', 'get_path', 'cfgpath']
+
+cfgpath = [os.path.join(os.path.dirname(sys.modules[__name__].__file__), '..', 'site.cfg')]
+'Path list to search for configuration files.'
 
 import logging
 class DisableLogger():
@@ -109,3 +122,19 @@ def display_header(filename, status=None):
     display(logo)
     display(revision_status(filename, status=status))
     display(codetoggle)
+
+def get_path(name):
+    '''Get path name info from site.cfg file in root directory.
+
+    If a path does not exist, it will be created.
+    '''
+    conf = configparser.ConfigParser()
+    cfgfile = conf.read(cfgpath)
+    if cfgfile:
+        print("Reading config file with path definitions: {}".format(cfgfile))
+    else:
+        raise Exception("No config file with path specifications found. File must be called 'site.py' and be located in one of the following directories: {}".format(cfgpath))
+    path = conf.get("Path", name)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
